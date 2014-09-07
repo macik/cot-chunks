b508b71998a2231fbe2d06f07fbcc10d935b2b1c
Andrew Matsovkin <macik.spb@gmail.com>
2014-09-07 04:02:16 +0400

    Added chunk params parser

--- plugins/chunks/chunks.global.php
+++ plugins/chunks/chunks.global.php
@@ -26,13 +26,16 @@ XTemplate::init(array(
 ));
 $chunks_test_string = 'Chunks work!';
 // -----------------------------------------------------------
-
-class_exists('Cotpl_ethandler') && $chunk_handler = new Cotpl_ethandler('`\{(CHUNK):([\w\.\-]+)\s*(.*?)\}`', 'chunk_parse');
+$tag_token = 'CHUNK';
+if (class_exists('Cotpl_ethandler'))
+{
+	$chunk_handler = new Cotpl_ethandler('`\{('.$tag_token.'):\s*([\w\.\-]+)(?:\s*\(?)(.*?)\)?\}`', 'chunk_parse');
+}
 
 // register extender
 XTemplate::set_extender(
 	array(
-		'CHUNK' => $chunk_handler,
+		$tag_token => $chunk_handler,
 	)
 );
 
@@ -73,12 +76,100 @@ function chunk_from_file($chunk_name){
 }
 
 /**
+ * Parse tag parameters and change placeholders
+ * @param string $chunk Chunk text body
+ * @param string $param_str Parameters string
+ * @return string Parsed Tag content
+ */
+function chunk_parse_params($chunk, $param_str)
+{
+		// $param_str = trim($param_str );
+	if (strpos($param_str, '=') === false)
+	{
+		// for unnamed params use fast str_getcsv
+		$params = str_getcsv($param_str, ',', '}'); // «}» used to save quotes
+	}
+	else
+	{
+		// named parameters
+		/*
+		 * preg_match_all("`\s*(?:([a-z][\w\d]+)(?:\s*=\s*))?([^\,]+?)\s*\,\s*`i", $param_str . ',', $p); $params = array(); foreach ($pp[1] as $key => $name) { if ($name) { $params[$name] = $pp[2][$key]; } else { $params[] = $pp[2][$key]; } }
+		 */
+
+		// TODO: make compare char-by-char parser vs regexp
+		foreach (str_split($param_str.',') as $idx => $char)
+		{
+			if (! $quoted)
+			{
+				if ($char == ' ') continue;
+				if ($char == '=')
+				{
+					$name = $token;
+					$token = '';
+				}
+				elseif ($char == ',')
+				{
+					$val = $token;
+					$token = '';
+					if ($name)
+					{
+						$params[$name] = $val;
+					}
+					else
+					{
+						$params[] = $val;
+					}
+				}
+				elseif (strpos('"\'`', $char) !== false)
+				{
+					$quoted = true;
+					$token .= $char;
+				}
+				else $token .= $char;
+			}
+			else
+			{
+				if (strpos('"\'`', $char) !== false)
+				{
+					$quoted = false;
+				}
+				$token .= $char;
+			}
+		}
+	}
+
+	// prepare values
+	foreach ($params as $key => &$val)
+	{
+		$val = trim($val);
+		if (! is_numeric($val))
+		{
+			$first_char = substr($val, 0, 1);
+			if (strpos('"\'`', $first_char) !== false)
+			{
+				// strip quotes
+				if (substr($val, - 1) == $first_char)
+				{
+					$val = str_replace($first_char, '', $val);
+				}
+			}
+			else
+			{
+				// made TPL tag reference
+				$val = '{' . $val . '}';
+			}
+		}
+	}
+	return cot_rc($chunk, $params);
+}
+
+/**
  * Template Chunks parser
  * @param Cotpl_etblock $block instance of block containing parsed tag
  * @param array $tag array of PCRE search results (see tag definition in set_extender):
  *     $tag[1] — name of tag token, example `CHUNK`
  *     $tag[2] — Name of chunk
- *     $tag[3] — rest of all
+ *     $tag[3] — rest of all (parameters)
  * @return string Parsed chunk
  */
 function chunk_parse(Cotpl_etblock $block, $tag)
@@ -101,6 +192,7 @@ function chunk_parse(Cotpl_etblock $block, $tag)
 		}
 		if (! empty($chunk))
 		{
+			if (trim($tag[3])) $chunk = chunk_parse_params($chunk, $tag[3]);
 			$parents = $block->parents;
 			$parents[] = $etag->id;
 			$sub_block = new Cotpl_etblock($parents);
